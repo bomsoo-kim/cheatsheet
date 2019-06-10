00c1bf1c64f19a6ae9fac995df462078615cb3bc
bomsoo-kim <46009363+bomsoo-kim@users.noreply.github.com>
2019-06-09 22:16:49 -0400

    Update save_load_multiple_variables.py

--- save_load_multiple_variables.py
+++ save_load_multiple_variables.py
@@ -1,14 +1,12 @@
 # ref) https://stackoverflow.com/questions/6568007/how-do-i-save-and-restore-multiple-variables-in-python
 import pickle
 
-# Saving the objects:
-def save_variables(list_of_variables, filename = 'backup_variables.pkl'):
-    with open(filename, 'wb') as f:  # Python 3: open(..., 'wb')
+def save_variables(list_of_variables, filename = 'backup_variables.pkl'): # Saving the objects
+    with open(filename, 'wb') as f:
         pickle.dump(list_of_variables, f) # list_of_variables = [var1, var2, var3, ...]
 
-# Getting back the objects:
-def load_variables(filename = 'backup_variables.pkl'):
-    with open(filename, 'rb') as f:  # Python 3: open(..., 'rb')
+def load_variables(filename = 'backup_variables.pkl'): # Getting back the objects
+    with open(filename, 'rb') as f:
         list_of_variables = pickle.load(f)  # list_of_variables = [var1, var2, var3, ...]
     return list_of_variables
  
@@ -17,6 +15,7 @@ a0 = 'hello world'
 b0 = ['a','b','c']
 c0 = [1,2,3,4,5]
 
+#-----------------------------------------
 save_variables([a0,b0,c0]) # Saving the objects
 
 #-----------------------------------------
